eaafc30eceb9b0f5737cf9fb3fd9f84ad9921125
dipson level <dipsonlevel@gmail.com>
2024-06-21 20:37:59 +0100

    first commit

---
+++ gunse.js
@@ -0,0 +1,9 @@
+// const friends =["ayanloye segun","aladegoroye adeola","olatunji nicolas","adegbite adebayo","songhonron hannah"]
+
+// console.log(friends)
+
+
+const friends =["ayanloye segun","aladegoroye adeola","olatunji nicolas","adegbite adebayo","songhonron hannah"]
+
+const bestfriends = friends[4]
+console.log(bestfriends)
