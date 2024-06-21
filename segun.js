1daaa382a7e40006e5f87503297450310f63567d
dipson level <dipsonlevel@gmail.com>
2024-06-21 17:14:14 +0100

    first commit

---
+++ segun.js
@@ -0,0 +1,13 @@
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width
+    return area
+  }
+  console.log(calculateAreaOfRectangle(5,6))
+  console.log(calculateAreaOfRectangle(6,7))
+//   
+
+areaofrectangle = (l,b)=>{
+    const multiply = (l*b)
+    return multiply
+}
+console.log(areaofrectangle(6,7))
